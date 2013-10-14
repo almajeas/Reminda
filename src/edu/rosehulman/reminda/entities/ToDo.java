d6763f3bebe3a4a1e18f8e2c4d3ca5155feb4e23
Omar Abdel <omar.abdelrahman@gmail.com>
2013-10-14 00:47:34 -0400

    added date to long

--- src/edu/rosehulman/reminda/entities/ToDo.java
+++ src/edu/rosehulman/reminda/entities/ToDo.java
@@ -1,6 +1,6 @@
 package edu.rosehulman.reminda.entities;
 
-import java.util.Date;
+import java.util.GregorianCalendar;
 
 import android.widget.DatePicker;
 import android.widget.TimePicker;
@@ -9,32 +9,33 @@ public class ToDo {
 	private String mTitle;
 	private long mId;
 	private String mMessage;
-	private long mDate; //days since jan1 1970
-	private long mTime; //min since midnight
-	
+	private long mDate; // days since jan1 1970
+	private long mTime; // min since midnight
+
 	public ToDo(String title, String message, long date, long time) {
 		mTitle = title;
 		mMessage = message;
 		mDate = date;
 		mTime = time;
 	}
-	
+
 	public ToDo(String title, String message, DatePicker date, TimePicker time) {
 		mTitle = title;
 		mMessage = message;
-		mDate = 0;
-		mTime = 0;
-		//setDateToLong(date, time);
-	}
-
-
-	private void setDateToLong(DatePicker date, TimePicker time) {
-		Date newDate;
-		
+		setDateToLong(date, time);
 	}
 
 	public ToDo() {
 		// TODO Auto-generated constructor stub
+	}
+
+	private void setDateToLong(DatePicker date, TimePicker time) {
+		GregorianCalendar newDate = new GregorianCalendar(date.getYear(),
+				date.getMonth(), date.getDayOfMonth());
+		mDate = newDate.getTimeInMillis();
+		newDate = new GregorianCalendar(0, 0, 0, time.getCurrentHour(),
+				time.getCurrentMinute());
+		mTime = newDate.getTimeInMillis();
 	}
 
 	public String getTitle() {
