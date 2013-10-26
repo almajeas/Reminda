2e74231feec79fdc83aa0397b99591e4afb9a53a
Ali Almajed <almajeas@rose-hulman.edu>
2013-10-26 19:55:53 -0400

    added laps to stopwatch

--- src/edu/rosehulman/reminda/StopWatchFragment.java
+++ src/edu/rosehulman/reminda/StopWatchFragment.java
@@ -1,6 +1,7 @@
 package edu.rosehulman.reminda;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
@@ -9,23 +10,33 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class StopWatchFragment extends Fragment implements OnClickListener {
-	private enum STATE {FRESH, RUNNING, PAUSED};
+	private enum STATE {
+		FRESH, RUNNING, PAUSED
+	};
+
 	private STATE mState;
+	private Context mContext;
 	private TextView mTimeTextView;
-	private Button mStartButton, mResetButton;
+	private Button mStartButton, mResetButton, mLapButton;
+	private LinearLayout mLapsLayout;
+	private ScrollView mLapsScrollView;
 	private long mTime = 0;
 	private long mStartTime = 0;
 	private long mTotalTime = 0;
 	private long mTimeSwap = 0;
+	private int mLapsCounter = 1;
+	private long mLapTime = 0;
 	private Handler mHandler;
-	private Runnable updateTimer = new Runnable(){
-		public void run(){
+	private Runnable updateTimer = new Runnable() {
+		public void run() {
 			mTime = SystemClock.uptimeMillis() - mStartTime;
 			mTotalTime = mTimeSwap + mTime;
-			timeDisplay();
+			mTimeTextView.setText(timeFormatString(mTotalTime));
 			mHandler.postDelayed(this, 0);
 		}
 	};
@@ -34,35 +45,40 @@ public class StopWatchFragment extends Fragment implements OnClickListener {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.activity_stop_watch, container, false);
+		mContext = this.getActivity();
 		mState = STATE.FRESH;
 		mTimeTextView = (TextView) v.findViewById(R.id.timeTextView);
-		mStartButton = (Button) v.findViewById(R.id.StartButton);
+		mStartButton = (Button) v.findViewById(R.id.startButton);
 		mStartButton.setOnClickListener(this);
 		mResetButton = (Button) v.findViewById(R.id.resetButton);
 		mResetButton.setOnClickListener(this);
 		mResetButton.setEnabled(false);
+		mLapButton = (Button) v.findViewById(R.id.lapButton);
+		mLapButton.setOnClickListener(this);
+		mLapsLayout = (LinearLayout) v.findViewById(R.id.lapsLayout);
+		mLapsScrollView = (ScrollView) v.findViewById(R.id.lapsScrollView);
 		mHandler = new Handler();
 		return v;
 	}
 
 	@Override
 	public void onClick(View v) {
-		switch(v.getId()){
-		case R.id.StartButton:
-			if(mState == STATE.FRESH || mState == STATE.PAUSED){
+		switch (v.getId()) {
+		case R.id.startButton:
+			if (mState == STATE.FRESH || mState == STATE.PAUSED) {
 				mStartTime = SystemClock.uptimeMillis();
 				mState = STATE.RUNNING;
 				mResetButton.setEnabled(true);
 				mStartButton.setText(getResources().getString(R.string.pause));
 				mHandler.postDelayed(updateTimer, 0);
-			}else if(mState == STATE.RUNNING){
+			} else if (mState == STATE.RUNNING) {
 				mState = STATE.PAUSED;
 				mStartButton.setText(getString(R.string.start));
 				mTimeSwap = mTotalTime;
 				mHandler.removeCallbacks(updateTimer);
 			}
 			break;
-			
+
 		case R.id.resetButton:
 			mState = STATE.FRESH;
 			mHandler.removeCallbacks(updateTimer);
@@ -72,17 +88,51 @@ public class StopWatchFragment extends Fragment implements OnClickListener {
 			mStartTime = 0;
 			mTotalTime = 0;
 			mTimeSwap = 0;
-			timeDisplay();
+			mLapsCounter = 1;
+			mLapTime = 0;
+			mTimeTextView.setText(timeFormatString(mTotalTime));
+			mLapsLayout.removeAllViews();
+			break;
+		case R.id.lapButton:
+			mResetButton.setEnabled(true);
+			addLap();
 			break;
 		}
-		
+
 	}
-	
-	private void timeDisplay() {
-		int seconds = (int) (mTotalTime/1000);
+
+	private void addLap() {
+		LayoutInflater inflater = (LayoutInflater) this.mContext
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LinearLayout item = (LinearLayout) inflater.inflate(R.layout.lap_item,
+				null);
+		int[] counterDepth = { 0 };
+		int[] timeDepth = { 2 };
+
+		TextView tCounter = (TextView) expandTV(item, counterDepth);
+		TextView tTime = (TextView) expandTV(item, timeDepth);
+		tCounter.setText(String.format("%d.", mLapsCounter++));
+
+		mLapTime = mTotalTime - mLapTime;
+		tTime.setText(timeFormatString(mLapTime));
+		mLapTime = mTotalTime;
+		mLapsLayout.addView(item);
+		mLapsLayout.refreshDrawableState();
+		mLapsScrollView.fullScroll(View.FOCUS_DOWN);
+	}
+
+	private View expandTV(LinearLayout root, int[] depths) {
+		for (int i = 0; i < depths.length - 1; i++) {
+			root = (LinearLayout) root.getChildAt(depths[i]);
+		}
+		return root.getChildAt(depths[depths.length - 1]);
+	}
+
+	private String timeFormatString(long time) {
+		int seconds = (int) (time / 1000);
 		int minutes = seconds / 60;
 		seconds = seconds % 60;
-		int milliseconds = (int) (mTotalTime %1000);
-		mTimeTextView.setText(String.format("%02d:%02d:%03d",minutes,seconds,milliseconds ));
+		int milliseconds = (int) (time % 1000);
+		return String.format("%02d:%02d:%03d", minutes, seconds, milliseconds);
 	}
 }
