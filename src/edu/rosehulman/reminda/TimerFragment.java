9c6dbb18aa6f108321d5752f3319af9dc3c483b6
Ali Almajed <almajeas@rose-hulman.edu>
2013-10-30 13:13:16 -0400

    added notification for timer onFinish()

--- src/edu/rosehulman/reminda/TimerFragment.java
+++ src/edu/rosehulman/reminda/TimerFragment.java
@@ -1,9 +1,17 @@
 package edu.rosehulman.reminda;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,14 +23,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class TimerFragment extends Fragment implements OnClickListener {
+	private Context mContext;
+	private Button mStartButton, mCancelButton;
+	private NumberPicker mHr, mMin, mSec;
+	private EditText mMessage;
+	private TextView mTimeText;
+	private Handler mHandler;
+	private CountDownTimer cdt;
+	private long mTimeLeft;
+	private int mNotificationId = 499;
+	
 
-	Button mStartButton, mCancelButton;
-	NumberPicker mHr, mMin, mSec;
-	EditText mMessage;
-	TextView mTimeText;
-	Handler mHandler;
-	CountDownTimer cdt;
-	long mTimeLeft;
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+		this.mContext = activity;
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -75,6 +92,7 @@ public class TimerFragment extends Fragment implements OnClickListener {
 				@Override
 				public void onFinish() {
 					Toast.makeText(getActivity(), "Timer is done", Toast.LENGTH_LONG).show();
+					triggerNotification();
 					mStartButton.setEnabled(true);
 					mCancelButton.setEnabled(false);
 
@@ -91,6 +109,38 @@ public class TimerFragment extends Fragment implements OnClickListener {
 			break;
 		}
 
+	}
+	
+	private void triggerNotification(){
+		Notification.Builder mBuilder =
+		        new Notification.Builder(getActivity())
+		        .setSmallIcon(R.drawable.reminda_notification)
+		        .setContentTitle("Reminda Timer")
+		        .setContentText("Timer Ended");
+		// Creates an explicit intent for an Activity in your app
+		Intent resultIntent = new Intent(mContext, RemindaActivity.class);
+
+		// The stack builder object will contain an artificial back stack for the
+		// started Activity.
+		// This ensures that navigating backward from the Activity leads out of
+		// your application to the Home screen.
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(mContext);
+		// Adds the back stack for the Intent (but not the Intent itself)
+		stackBuilder.addParentStack(RemindaActivity.class);
+		// Adds the Intent that starts the Activity to the top of the stack
+		stackBuilder.addNextIntent(resultIntent);
+		PendingIntent resultPendingIntent =
+		        stackBuilder.getPendingIntent(
+		            0,
+		            PendingIntent.FLAG_UPDATE_CURRENT
+		        );
+		mBuilder.setContentIntent(resultPendingIntent);
+		NotificationManager mNotificationManager =
+		    (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+		// mId allows you to update the notification later on.
+		mNotificationManager.notify(mNotificationId, mBuilder.build());
+		
+		
 	}
 
 }
